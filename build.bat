60fb14e868a2ea5a8c5cde6eb2ae205248575ad1
Hitesh Patel <h.patel@f5.com>
2015-07-08 19:03:56 -0500

    Added build.bat so template can be built via CLI on Windows.

---
+++ build.bat
@@ -0,0 +1,17 @@
+@echo off
+PATH=%PATH%;c:\Python27;c:\Python26
+
+mkdir tmp
+
+echo Generating APL...
+python util/build_apl.py src\presentation_layer.json > tmp\apl.build
+
+echo Generating docs...
+python util/build_doc.py src\presentation_layer.json > OPTIONS.md
+
+echo Assembling template...
+python util\build_tmpl.py %cd%
+
+del tmp\apl.build
+rmdir tmp
+
