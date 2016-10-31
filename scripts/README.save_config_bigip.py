37ca9dae6a41e45791da91c763323722dad83030
Hitesh Patel <h.patel@f5.com>
2016-10-31 15:55:11 -0500

    Additional helper script to help with CI/CD integration

---
+++ scripts/README.save_config_bigip.py
@@ -0,0 +1,7 @@
+save_config_bigip.py
+	Save the config a BIG-IP system using the iControl-REST API
+
+This script saves the config on the specified BIG-IP device
+
+For further options please run the script with the --help argument
+
